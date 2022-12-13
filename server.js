e81f2a7f27aac38de606437d13a7edb834ecf480
Svencap <Ayzekmanduar@mail.ru>
2022-12-14 06:44:02 +0900

    change mongoDB url

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 import express from "express";
 import UserModel from "./models/User.js";
 import router from "./router.js";
-import * as dotenv from 'dotenv'
-dotenv.config()
-
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,13 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/v1", router);
 
-
 const runApp = async () => {
   try {
     await mongoose
-      .connect(
-        "mongodb+srv://Svencap:Arhara256@cluster0.5x7b7kl.mongodb.net/testdb?retryWrites=true&w=majority"
-      )
+      .connect(process.env.DB_URL)
       .then(() => console.log("connection to Mongo"));
 
     app.listen(port, () => console.log(`Listening on port ${port}`));
