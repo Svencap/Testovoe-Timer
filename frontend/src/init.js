0ffdc2e7e515d3bea58b9f7dec88529540e1a523
Svencap <Ayzekmanduar@mail.ru>
2022-12-16 05:43:54 +0900

    add redux store, delete strict mode

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { io } from "socket.io-client";
+import store from './state/store.js';
+
+import { Provider } from "react-redux";
 
 const init = async () => {
-
   const socket = io();
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
   );
 };
 export default init;
